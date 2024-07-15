eaac7fbdd35e906f8bd732806f79d1a0b2cfd93f
abyand <abyand71@gmail.com>
2024-07-15 16:54:00 +0800

    deal cards evenly to players and add sort and validation logic

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net"
 	"os"
+	"sort"
 	"strings"
 	"sync"
+	"time"
 )
+
+type Card struct {
+	Suit     string
+	Value    string
+	SuitNum  int // 0 => "♥", 1 => "♦", 2 => "♣", 3 => "♠"
+	ValueNum int // 2-10 => "2"-"10", 11 => "J", 12 => "Q", 13 => "K", 14 => "A"
+}
 
 type Client struct {
 	Conn     net.Conn
 	Username string
 	Ready    bool
+	Hand     []Card
 }
 
 var (
@@ -85,8 +97,20 @@ func handleConnection(client Client, socketID string) {
 				}
 			}
 			if allReady && len(connections[socketID]) == 4 {
+				deck := dealCards(connections[socketID])
 				for i := range connections[socketID] {
-					connections[socketID][i].Conn.Write([]byte("Yeay all the players are ready, let's get this game started\n"))
+					cardsData, _ := json.Marshal(connections[socketID][i].Hand)
+					msg := fmt.Sprintf(
+						"All players are ready, game has started. Here are your cards: %s\nCards remaining in deck: %d\n",
+						string(cardsData),
+						len(deck),
+					)
+					for j, otherClient := range connections[socketID] {
+						if j != i {
+							msg += fmt.Sprintf("Total cards %s has: %d\n", otherClient.Username, len(otherClient.Hand))
+						}
+					}
+					connections[socketID][i].Conn.Write([]byte(msg))
 				}
 			}
 			mutex.Unlock()
@@ -152,4 +176,53 @@ func removeConnection(slice []Client, client Client) []Client {
 		}
 	}
 	return slice
+}
+
+func createDeck() []Card {
+	suits := []string{"♥", "♦", "♣", "♠"}
+	values := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
+	valueNums := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+	var deck []Card
+
+	for i, suit := range suits {
+		for j, value := range values {
+			deck = append(deck, Card{Suit: suit, Value: value, SuitNum: i, ValueNum: valueNums[j]})
+		}
+	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(deck), func(i, j int) { deck[i], deck[j] = deck[j], deck[i] })
+	return deck
+}
+
+func dealCards(clients []Client) []Card {
+	deck := createDeck()
+	for i := range clients {
+		clients[i].Hand = deck[:13] // Deal 13 cards to each player
+		sort.Slice(clients[i].Hand, func(j, k int) bool {
+			if clients[i].Hand[j].SuitNum == clients[i].Hand[k].SuitNum {
+				return clients[i].Hand[j].ValueNum < clients[i].Hand[k].ValueNum
+			}
+			return clients[i].Hand[j].SuitNum < clients[i].Hand[k].SuitNum
+		}) // Sort the cards
+		if !validateHand(clients[i].Hand) { // Validate the hand
+			fmt.Println("Invalid hand, dealing again...")
+			// If the hand is invalid, deal again.
+			// Note: This can potentially lead to an infinite loop if the deck can't produce a valid hand.
+			return dealCards(clients)
+		}
+		deck = deck[13:] // Remove the dealt cards from the deck
+	}
+	return deck // Return remaining deck
+}
+
+func validateHand(hand []Card) bool {
+	suitCount := make(map[int]bool)
+	hasCardBiggerThan10 := false
+	for _, card := range hand {
+		suitCount[card.SuitNum] = true
+		if card.ValueNum > 10 {
+			hasCardBiggerThan10 = true
+		}
+	}
+	return len(suitCount) == 4 && hasCardBiggerThan10
 }
